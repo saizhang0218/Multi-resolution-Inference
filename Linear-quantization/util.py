84e02ad28c13507758d44bc542c6c373e6d0a61f
saizhang0218 <sai.zhang@mail.utoronto.ca>
2021-01-18 13:15:21 -0800

    Update util.py

--- Linear-quantization/util.py
+++ Linear-quantization/util.py
@@ -20,9 +20,7 @@ import torch.nn.functional as F
 def loss_teacher_student(student_outputs, labels, teacher_outputs):
     alpha = 0.5
     T = 4.0
-    KD_loss = nn.KLDivLoss()(F.log_softmax(student_outputs/T, dim=1),
-                             F.softmax(teacher_outputs/T, dim=1)) * (alpha * T * T) + \
-              F.cross_entropy(student_outputs, labels) * (1. - alpha)
+    KD_loss = nn.KLDivLoss()(F.log_softmax(student_outputs/T, dim=1), F.softmax(teacher_outputs/T, dim=1)) * (alpha * T * T) + F.cross_entropy(student_outputs, labels) * ((1. - alpha)/2) + F.cross_entropy(teacher_outputs, labels) * ((1. - alpha)/2)
 
     return KD_loss
 
