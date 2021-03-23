4e00faa694033179adf28163093673381b2d41f9
saizhang0218 <sai.zhang@mail.utoronto.ca>
2021-03-22 23:19:32 -0700

    Update main.py

--- Term-quantization/main.py
+++ Term-quantization/main.py
@@ -47,7 +47,7 @@ parser.add_argument('--dist-url', default='tcp://224.66.41.62:23456', type=str, 
 parser.add_argument('--dist-backend', default='nccl', type=str, help='distributed backend')
 parser.add_argument('--seed', default=None, type=int, help='seed for initializing training. ')
 parser.add_argument('--gpu', default=None, type=int, help='GPU id to use.')
-parser.add_argument('-lt', '--loss_type', default='joint', help='two options: joint, distill')
+parser.add_argument('-lt', '--loss_type', default='distill', help='two options: distill, joint')
 parser.add_argument('--multiprocessing-distributed', action='store_true',
                     help='Use multi-processing distributed training to launch '
                          'N processes per node, which has N GPUs. This is the '
